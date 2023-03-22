d2c6dfc85a74a32c092f9af1f9412dc818b4f13a
Caballero018 <alejoxcaballero18@gmail.com>
2023-03-22 10:56:03 -0500

    Readme of the project

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,15 +1,37 @@
 #!/usr/bin/python3
-"Box"
-
 
 def canUnlockAll(boxes):
-    "Box"
     counter = 0
+    dic_box = {}
     for i in range(len(boxes) - 1):
         if len(boxes[i]) > 1:
             counter += 1
+        if boxes[i] == []:
+            k = '[]'
+            if k not in dic_box.keys():
+                dic_box[k] = 0
+            if k in dic_box.keys():
+                dic_box[k] += 1
 
-
-
+        for j in range(len(boxes[i])):
+            key = str(boxes[i][j])
+            if key not in dic_box.keys():
+                dic_box[key] = 0
+            if key in dic_box.keys():
+                if key not in dic_box.keys():
+                    dic_box[key] = 1
+                elif key in dic_box.keys():
+                    dic_box[key] += 1
     if counter == 0:
         return True
+    counter = 0
+    for key in dic_box.keys():
+        if dic_box[key] >= 1 and key != '[]':
+            counter += 1
+    if counter == len(dic_box):
+        return True
+
+    if counter != len(dic_box):
+        return False
+    
+
