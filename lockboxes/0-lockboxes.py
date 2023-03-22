ba49c786ad0c3d34890be74f9f0da094bb0e646b
Caballero018 <alejoxcaballero18@gmail.com>
2023-03-22 13:43:52 -0500

    Method that determines if all the boxes can be opened.

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -4,38 +4,13 @@
 
 def canUnlockAll(boxes):
     "GG"
-
-    counter = 0
-    dic_box = {}
-    for i in range(len(boxes) - 1):
-        if len(boxes[i]) > 1:
-            counter += 1
-        if boxes[i] == []:
-            k = '[]'
-            if k not in dic_box.keys():
-                dic_box[k] = 0
-            if k in dic_box.keys():
-                dic_box[k] += 1
-
-        for j in range(len(boxes[i])):
-            key = str(boxes[i][j])
-            if key not in dic_box.keys():
-                dic_box[key] = 0
-            if key in dic_box.keys():
-                if key not in dic_box.keys():
-                    dic_box[key] = 1
-                elif key in dic_box.keys():
-                    dic_box[key] += 1
-    if counter == 0:
+    unlocked = [0]
+    for box_iterator, box in enumerate(boxes):
+        if not box:
+            continue
+        for key in box:
+            if key < len(boxes) and key not in unlocked and key != box_iterator:
+                unlocked.append(key)
+    if len(boxes) == len(unlocked):
         return True
-    counter = 0
-    for key in dic_box.keys():
-        if dic_box[key] >= 1 and key != '[]':
-            counter += 1
-    if counter == len(dic_box):
-        return True
-
-    if counter != len(dic_box):
-        return False
-    
-
+    return False
