b36f2e07b6aa6995e5ba5869a2beddaf2f0d1255
Caballero018 <alejoxcaballero18@gmail.com>
2023-03-22 09:58:15 -0500

    Readme of the project

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+"Box"
+
 
 def canUnlockAll(boxes):
+    "Box"
     counter = 0
     for i in range(len(boxes) - 1):
         if len(boxes[i]) > 1:
             counter += 1
-            
+
 
 
     if counter == 0:
