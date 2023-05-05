6636cae1c51d8bc4a608d0cb79305f78a7b3991c
Caballero018 <alejoxcaballero18@gmail.com>
2023-05-05 00:31:22 -0500

    CHanges

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,39 +1,47 @@
 #!/usr/bin/python3
-"""stats module
+"""
+Module that parses a log and prints stats to stdout
 """
 from sys import stdin
 
+status_codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
 
-codes = {'200': 0, '301': 0, '400': 0, '401': 0,
-         '403': 0, '404': 0, '405': 0, '500': 0}
 size = 0
 
 
-def print_info():
-    """print_info method print needed info
-
-    Args:
-        codes (dict): code status
-        size (int): size of files
-    """
+def print_stats():
+    """Prints the accumulated logs"""
     print("File size: {}".format(size))
-    for key, val in sorted(codes.items()):
-        if val > 0:
-            print("{}: {}".format(key, val))
+    for status in sorted(status_codes.keys()):
+        if status_codes[status]:
+            print("{}: {}".format(status, status_codes[status]))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
+    count = 0
     try:
-        for i, line in enumerate(stdin, 1):
+        for line in stdin:
             try:
-                info = line.split()
-                size += int(info[-1])
-                if info[-2] in codes.keys():
-                    codes[info[-2]] += 1
+                items = line.split()
+                size += int(items[-1])
+                if items[-2] in status_codes:
+                    status_codes[items[-2]] += 1
             except:
                 pass
-            if not i % 10:
-                print_info()
+            if count == 9:
+                print_stats()
+                count = -1
+            count += 1
     except KeyboardInterrupt:
-        print_info()
+        print_stats()
         raise
-    print_info()
+    print_stats()
